d0d0ef3e66b08e669b5fd6e62e880ad5b11fc9e1
Omega Terra Coriel <44127678+MissCoriel@users.noreply.github.com>
2024-10-31 13:55:04 -0500

    Initial Commit

---
+++ NPCToolKit/ModEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.ItemTypeDefinitions;
+
+namespace NPCToolKit
+{
+    internal class ModEntry : Mod
+    {
+        public Dictionary<string, string> ItemList = new Dictionary<string, string>();
+        public override void Entry(IModHelper helper)
+        {
+                
+        }
+        public void Extractor()
+        {
+
+            foreach (IItemDataDefinition itemType in ItemRegistry.ItemTypes)
+            {
+                foreach (string id in itemType.GetAllIds())
+                {
+                    Item item = ItemRegistry.Create(itemType.Identifier + id);
+                    if (item.canBeGivenAsGift())
+                    {
+                        ItemList.Add(item.Name, id);
+                    }
+                }
+            }
+        }
+    }
+}
